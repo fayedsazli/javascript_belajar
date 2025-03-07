7dd67c8730bd4df594dc88c5ac0bdb21f8458659
fayedsazli <fayedsazli07@gmail.com>
2025-03-07 17:08:26 +0800

    feat : Color Flipper training

--- color flipper/script.js
+++ color flipper/script.js
@@ -1,15 +1,12 @@
-const body = document.getElementsByTagName("body")[0]
+const latar =  document.getElementsByTagName("body")[0];
 
-function setColor(name){
-    body.style.backgroundColor = name ;
+function setColor(str){
+    latar.style.backgroundColor = str;
 }
 
-function randomCollor(){
-    const red = Math.round(Math.random()*255) ;
-    const blue = Math.round(Math.random()*255) ;
-    const green = Math.round(Math.random()*255) ;
-    
-    const color = `rgb(${red},${green},${blue})`;
-    body.style.backgroundColor = color;
+function randomColor(){
+    const red = Math.round(Math.random()*255);
+    const green = Math.round(Math.random()*255);
+    const blue = Math.round(Math.random()*255);
+    setColor(`rgb(${red}, ${green}, ${blue})`);
 }
-
