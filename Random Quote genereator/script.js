9ad5170c14dd29a63ed1ed04a9285d13f6edb546
fayedsazli <fayedsazli07@gmail.com>
2025-04-02 08:19:43 +0800

    new feat

---
+++ Random Quote genereator/script.js
@@ -0,0 +1,14 @@
+const quote = [
+    "Kesuksesan bukanlah akhir, kegagalan bukanlah hal yang fatal: yang penting adalah keberanian untuk melanjutkan.",
+    "Satu-satunya cara untuk melakukan pekerjaan hebat adalah dengan mencintai apa yang kamu lakukan.",
+    "Jangan menunggu waktu yang tepat, buatlah setiap waktu menjadi tepat.",
+    "Percayalah bahwa kamu bisa, maka kamu sudah setengah jalan menuju keberhasilan.",
+    "Kesuksesan biasanya datang kepada mereka yang terlalu sibuk untuk mencarinya.",
+    "Peluang tidak terjadi begitu saja. Kamu yang menciptakannya.",
+    "Kerja keras mengalahkan bakat ketika bakat tidak bekerja keras.",
+    "Batasanmu hanyalah imajinasimu sendiri.",
+    "Dorong dirimu sendiri, karena tidak ada orang lain yang akan melakukannya untukmu.",
+    "Mimpikan, harapkan, dan lakukan.",
+];
+
+console.log(quote);
